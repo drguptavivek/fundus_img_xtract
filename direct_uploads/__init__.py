983387e060ce4ee345a006337696ad7479da949f
Vivek Gupta <drguptavivek@yahoo.com>
2025-09-05 18:34:25 +0530

    simplify

--- direct_uploads/__init__.py
+++ direct_uploads/__init__.py
@@ -4,8 +4,6 @@ bp = Blueprint(
     "direct_uploads",
     __name__,
     template_folder="templates",
-    static_folder="static",
-    static_url_path="/static/direct_uploads",
 )
 
 @bp.record_once

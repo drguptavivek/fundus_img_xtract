f473ba12bf21bb64c5f4335a50abdc38b7fc02cd
Vivek Gupta <drguptavivek@yahoo.com>
2025-10-03 09:11:40 +0530

    refactored results BP to analytics BP

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -2,6 +2,6 @@
 
 from flask import Blueprint
 
-bp = Blueprint("results", __name__, url_prefix="/results")
+bp = Blueprint("analytics", __name__, url_prefix="/analytics")
 
 from . import routes  # noqa: E402,F401
